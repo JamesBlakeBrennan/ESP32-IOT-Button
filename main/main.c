9780b62b6ab7078df646c04553c986dba6a7c5a6
James Brennan <73494948+jabr6740@users.noreply.github.com>
2021-01-09 15:59:22 -0700

    Button Press and Deep Sleep
    Chip will deep sleep until button is pressed

--- main/main.c
+++ main/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_sleep.h"
+#include "driver/rtc_io.h"
 
+#define PIN_BUTTON 15
+#define PIN_LED 2
 
-
+RTC_DATA_ATTR int buttonPresses = 0;
 void app_main(void)
 {
-  printf("Hello world!\n");
+  rtc_gpio_pullup_en(PIN_BUTTON);
+  rtc_gpio_pulldown_dis(PIN_BUTTON);
+
+  esp_sleep_enable_ext0_wakeup(PIN_BUTTON, 0);
+  printf("going into deepsleep, button has been pressed %d times\n", buttonPresses++);
+
+  esp_deep_sleep_start();
+
   
 }
