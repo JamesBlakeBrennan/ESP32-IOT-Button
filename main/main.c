baceebb42e95d1b12fa1e090109be07e838e2c0e
James Brennan <73494948+jabr6740@users.noreply.github.com>
2021-01-10 15:19:05 -0700

    Button Reconnects after sleep
    Wifi is now reconnecting after wake up

--- main/main.c
+++ main/main.c
@@ -15,27 +15,39 @@
 #include "esp_smartconfig.h"
 #include "esp_sleep.h"
 #include "driver/rtc_io.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 //the following code is taken from the ESP touch example provided with the esp idf
 
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t s_wifi_event_group;
+static EventGroupHandle_t wake_up_wifi_event_group;
 
 /* The event group allows multiple bits for each event,
    but we only care about one event - are we connected
    to the AP with an IP? */
 static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
+#define WIFI_CONNECTED_BIT BIT2
+#define WIFI_FAIL_BIT      BIT3
+
+
 static const char *TAG = "smartconfig_example";
+static const char *TAG1 = "wifi station";
+
+static int s_retry_num = 0;
+
 
 #define PIN_BUTTON 15
 #define PIN_LED 2
+#define EXAMPLE_ESP_MAXIMUM_RETRY  5
 
 xSemaphoreHandle wifiDone;
 RTC_DATA_ATTR int buttonPresses = 0;
 RTC_DATA_ATTR int isRunning = 0;
-RTC_DATA_ATTR unsigned char RTCssid[33]  = {{ 0 }};
-RTC_DATA_ATTR unsigned char RTCpassword[65] = {{ 0 }};
+RTC_DATA_ATTR uint8_t RTCssid[32]  = { 0 };
+RTC_DATA_ATTR uint8_t RTCpassword[64] = { 0 };
 RTC_DATA_ATTR int wifimode = 0;
 static void smartconfig_example_task(void * parm);
 void running(void);
@@ -125,11 +137,109 @@ static void smartconfig_example_task(void * parm)
     }
 }
 
+static void event_handler_wakeup(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG1, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(wake_up_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG1,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG1, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(wake_up_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    wake_up_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler_wakeup, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler_wakeup, NULL));
+    wifi_config_t wifi_config = {
+        
+        .sta = {
+            .ssid = {0},
+            .password = {0},
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    memcpy(wifi_config.sta.ssid, RTCssid, sizeof(RTCssid));
+    memcpy(wifi_config.sta.password, RTCpassword, sizeof(RTCpassword));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler_wakeup() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(wake_up_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG1, "connected to ap SSID:%s password:%s",
+                 RTCssid, RTCpassword);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG1, "Failed to connect to SSID:%s, password:%s",
+                 RTCssid, RTCpassword);
+    } else {
+        ESP_LOGE(TAG1, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    
+    vEventGroupDelete(wake_up_wifi_event_group);
+}
+
 void startup(void)
 {
     if(isRunning == 0){
     ESP_ERROR_CHECK( nvs_flash_init() );
     initialise_wifi();
+    }
+    else{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG1, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
     }
   
 }
@@ -139,25 +249,28 @@ void running(void)
     while(true)
     {
     if(isRunning == 0){
-    xEventGroupWaitBits(s_wifi_event_group, ESPTOUCH_DONE_BIT, false, true, portMAX_DELAY);
+        xEventGroupWaitBits(s_wifi_event_group, ESPTOUCH_DONE_BIT, false, true, portMAX_DELAY);
     isRunning = 1;
     }
-    
+    else{
+        xEventGroupWaitBits(wake_up_wifi_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY );
+    }
     rtc_gpio_pullup_en(PIN_BUTTON);
     rtc_gpio_pulldown_dis(PIN_BUTTON);
 
     esp_sleep_enable_ext0_wakeup(PIN_BUTTON, 0);
     printf("going into deepsleep, button has been pressed %d times\n", buttonPresses++);
+    //esp_wifi_stop();
     esp_deep_sleep_start();
     }
+    
 }
 
 
 void app_main(void)
 {
   
-  esp_wifi_get_mode(wifimode);
-  printf("\n%d\n", wifimode);
+  
   printf("\n%d\n", isRunning);
   printf("\n%s\n", RTCssid);
   printf("\n%s\n", RTCpassword);
